5802a20714b9dd5f36ccf868c3937a1d706f1727
Erik Zaadi <erik.zaadi@gmail.com>
2011-04-17 20:50:04 +0300

    fixed directories

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-bashRC="$1" 
-if [[ "$bashRC" == "" ]]; then 
-	bashRC="bashRC"
-fi
-bashRC="~/.$bashRC"
 echo "dotFiles installation FTW!"
 if [[ ! -d ~/.vim && ! -L ~/.vim ]] ; then
     echo "Install vim first!" && exit 1
@@ -15,28 +10,28 @@ else
 	echo "adding dotFiles to vimrc" && echo '"dotFiles start\nsource ~/.vim_add\n"dotFiles end' >> ~/.vimrc
 fi
 
-if ( grep "dotFiles start" <$bashRC> -q ); then
+if ( grep "dotFiles start" ~/.bashrc -q ); then
 	echo "dotFiles found at bashrc"
 else
-	echo "adding dotFiles to bashrc" && echo '#dotFiles start\nif [ -f ~/.bash_add ]; then\nsource ~/.bash_add\nfi\n#dotFiles end' >> $bashRC
+	echo "adding dotFiles to bashrc" && echo '#dotFiles start\nif [ -f ~/.bash_add ]; then\nsource ~/.bash_add\nfi\n#dotFiles end' >> ~/.bashrc 
 fi
 
 if [[ -f ~/.bash_add ]]; then
 	echo "bash profile addon already exists"
 else
-	cp useroot/bash_add ~/.bash_add && echo "Copying bash profile addon"
+	cp userroot/bash_add ~/.bash_add && echo "Copying bash profile addon"
 fi
 
 if [[ -f ~/.vim_add ]]; then
 	echo "vim profile addon already exists"
 else
-	cp useroot/vim_add ~/.vim_add && echo "Copying vim profile addon"
+	cp userroot/vim_add ~/.vim_add && echo "Copying vim profile addon"
 fi
 
 if [[ -f ~/.git-completion.bash ]]; then
 	echo "git completion for bash already exists"
 else
-	cp useroot/git-completion.bash ~/.git-completion.bash && echo "Copying git completion for bash"
+	cp userroot/git-completion.bash ~/.git-completion.bash && echo "Copying git completion for bash"
 fi
 if [[ -f ~/.vim/vundle.git/.git/config ]]; then
 	echo "vundle already exists"
