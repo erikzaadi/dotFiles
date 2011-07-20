9456cf52f63836f689912412ff4a9f2cf346f0f3
Erik Zaadi <erikz@il.ibm.com>
2011-07-20 15:32:24 +0300

    using symlinks instead of copying

--- install.sh
+++ install.sh
@@ -1,41 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 echo "dotFiles installation FTW!"
-if [[ ! -d ~/.vim && ! -L ~/.vim ]] ; then
-    echo "Install vim first!" && exit 1
-fi
+mkdir -p ~/.vim
 
 if ( grep "dotFiles start" ~/.vimrc -q ); then
 	echo "dotFiles found at vimrc"
 else
-	echo "adding dotFiles to vimrc" && echo '"dotFiles start\nsource ~/.vim_add\n"dotFiles end' >> ~/.vimrc
+	echo "adding dotFiles to vimrc" && echo '"dotFiles start
+source ~/.vim_add
+"dotFiles end' >> ~/.vimrc
 fi
 
 if ( grep "dotFiles start" ~/.bashrc -q ); then
 	echo "dotFiles found at bashrc"
 else
-	echo "adding dotFiles to bashrc" && echo '#dotFiles start\nif [ -f ~/.bash_add ]; then\nsource ~/.bash_add\nfi\n#dotFiles end' >> ~/.bashrc 
+	echo "adding dotFiles to bashrc" && echo '#dotFiles start
+if [ -f ~/.bash_add ]; then 
+    source ~/.bash_add
+fi 
+#dotFiles end' >> ~/.bashrc 
 fi
 
-if [[ -f ~/.bash_add ]]; then
-	echo "bash profile addon already exists"
-else
-	cp userroot/bash_add ~/.bash_add && echo "Copying bash profile addon"
-fi
+current=$(pwd)
 
-if [[ -f ~/.vim_add ]]; then
-	echo "vim profile addon already exists"
-else
-	cp userroot/vim_add ~/.vim_add && echo "Copying vim profile addon"
-fi
+echo 'Adding bash addons'
+rm -rf ~/.bash_add
+ln -s $current/userroot/bash_add ~/.bash_add
 
-if [[ -f ~/.git-completion.bash ]]; then
-	echo "git completion for bash already exists"
-else
-	cp userroot/git-completion.bash ~/.git-completion.bash && echo "Copying git completion for bash"
-fi
-if [[ -f ~/.vim/vundle.git/.git/config ]]; then
+echo 'Adding vim addons'
+rm -rf ~/.vim_add
+ln -s $current/userroot/vim_add ~/.vim_add
+
+echo 'Adding git bash completion'
+rm -rf ~/.git-completion.bash
+ln -s $current/userroot/git-conpletion.bash ~/.git-completion.bash
+
+if [ -f ~/.vim/vundle.git/.git/config ]; then
 	echo "vundle already exists"
 else
 	echo "cloning vundle" && git clone https://github.com/gmarik/vundle.git ~/.vim/vundle.git
 fi
-echo "You're good to go! Restart you terminal and open vim, and run :BundleInstall"
+
+echo 'Installing vim bundles'
+vim -c BundleInstall!
+
+echo "You're good to go!"
