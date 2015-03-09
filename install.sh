9cbfd1cf78a814a1659c734a90c369aa58cda94b
erikzaadi <erik.zaadi@gmail.com>
2015-03-09 14:28:27 +0200

    auto install youcompleteme

--- install.sh
+++ install.sh
@@ -115,6 +115,7 @@ fi
 
 if [[ ! -f ~/.rvm/scripts/rvm ]]; then
     log_message "Installing rvm"
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable --ruby
     source ~/.rvm/scripts/rvm
 fi
@@ -125,6 +126,10 @@ npm i -g $(cat ${SCRIPT_BASE}/packages/node)
 log_message "Installing ruby gems.."
 gem install $(cat ${SCRIPT_BASE}/packages/ruby)
 
+log_message "Installing Vim Packages.."
 vim -c 'BundleInstall!' -c 'qa!'
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh
+cd -
 
 log_message "Done, great success!!1"
