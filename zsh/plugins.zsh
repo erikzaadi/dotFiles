cf416bb174b91e221a60ea62770bce63437d0b4e
erikzaadi <erik.zaadi@gmail.com>
2026-05-06 10:56:38 +0300

    Pimp my dotFiles

---
+++ zsh/plugins.zsh
@@ -0,0 +1,13 @@
+fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-completions" )
+source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-completions/zsh-completions.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/belak/zsh-utils/completion" )
+source "$HOME/Library/Caches/antidote/github.com/belak/zsh-utils/completion/completion.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/zlsun/solarized-man" )
+source "$HOME/Library/Caches/antidote/github.com/zlsun/solarized-man/solarized-man.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/zdharma-continuum/fast-syntax-highlighting" )
+source "$HOME/Library/Caches/antidote/github.com/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/lukechilds/zsh-better-npm-completion" )
+source "$HOME/Library/Caches/antidote/github.com/lukechilds/zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/Aloxaf/fzf-tab" )
+source "$HOME/Library/Caches/antidote/github.com/Aloxaf/fzf-tab/fzf-tab.plugin.zsh"
+export PATH="$HOME/Library/Caches/antidote/github.com/larz258/Zshopt:$PATH"
